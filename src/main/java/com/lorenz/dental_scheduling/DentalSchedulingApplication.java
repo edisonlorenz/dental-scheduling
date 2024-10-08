dd5ad480c5a1a2c6a2d0d6ff420f68dfc9382a42
edisonlorenz <edisonlorenz84@gmail.com>
2024-10-08 20:17:24 +0800

    Initial commit

---
+++ src/main/java/com/lorenz/dental_scheduling/DentalSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.lorenz.dental_scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lorenz/dental_scheduling/DentalSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenz.dental_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
